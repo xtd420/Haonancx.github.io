6fd37cdfd92ce0229414072e7e87be07bd57753a
guanrenchao <guanrenchao@yinghaihds.com>
2016-11-22 16:16:49 +0800

    add
    a

--- scripts/pithy.js
+++ scripts/pithy.js
@@ -19,13 +19,14 @@ $(function(){
       $(".navbar-toggle").click(function(){
       $(".nav").toggle(1000);
       });
-      
+
     var screenWidth=$(window).width();
     if(screenWidth <= 430){
         $(".navbar-toggle").css("display","inline-block");
         $(".nav").css({"margin":"0","position":"fixed","top":"55px","width":"100%","height":"auto","left":"0","background-image":"linear-gradient(to right, #207ce5 0, #26a9ff 100%)"});
     }else{
         $(".navbar-toggle").css("display","none");
+        $(".nav").css("display","block");
         
     }
 });
