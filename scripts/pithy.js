51c43ac69e3ff7b55ea1d44d1596fb1a3db786c1
guanrenchao <guanrenchao@yinghaihds.com>
2016-11-22 16:00:19 +0800

    chanegs

--- scripts/pithy.js
+++ scripts/pithy.js
@@ -18,7 +18,7 @@ $(function(){
     var screenWidth=$(window).width();
     if(screenWidth <= 430){
         $(".navbar-toggle").css("display","inline-block");
-        $(".nav").css("margin":"0","position":"fixed","top":"55px","width":"100%","height":"auto","left":"0","background-image":"linear-gradient(to right, #207ce5 0, #26a9ff 100%)");
+        $(".nav").css({"margin":"0","position":"fixed","top":"55px","width":"100%","height":"auto","left":"0","background-image":"linear-gradient(to right, #207ce5 0, #26a9ff 100%)"});
     }else{
         $(".navbar-toggle").css("display","none");
         
